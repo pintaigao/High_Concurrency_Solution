86e64ad1f73ffa4e9e605c316aba4f530696cf73
pintaigao <pintaigao@gmail.com>
2019-10-22 14:51:37 -0500

    Initiate

---
+++ 一. 高性能秒杀方案/安全的传输密码/MD5Util.java
@@ -0,0 +1,38 @@
+package com.nok.dragon.util.method;
+
+import org.apache.log4j.Logger;
+import org.springframework.util.CollectionUtils;
+
+import java.security.MessageDigest;
+
+/**
+ * Created by hzllb on 17/10/15.
+ */
+public class MD5Util {
+    private static final Logger logger = Logger.getLogger(MD5Util.class);
+    public final static String MD5(String s) {
+        char hexDigits[]={'0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f'};
+        try {
+            byte[] btInput = s.getBytes();
+            // 获得MD5摘要算法的 MessageDigest 对象
+            MessageDigest mdInst = MessageDigest.getInstance("MD5");
+            // 使用指定的字节更新摘要
+            mdInst.update(btInput);
+            // 获得密文
+            byte[] md = mdInst.digest();
+            // 把密文转换成十六进制的字符串形式
+            int j = md.length;
+            char str[] = new char[j * 2];
+            int k = 0;
+            for (int i = 0; i < j; i++) {
+                byte byte0 = md[i];
+                str[k++] = hexDigits[byte0 >>> 4 & 0xf];
+                str[k++] = hexDigits[byte0 & 0xf];
+            }
+            return new String(str);
+        } catch (Exception e) {
+            logger.error("",e);
+            return null;
+        }
+    }
+}
---
+++ 一. 高性能秒杀方案/安全的传输密码/SecureMethod.java
@@ -0,0 +1,293 @@
+package com.nok.dragon.util.method;
+
+import sun.misc.BASE64Decoder;
+import sun.misc.BASE64Encoder;
+
+import javax.crypto.Cipher;
+import java.security.*;
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.security.spec.X509EncodedKeySpec;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+
+/**
+ * Created by hzllb on 2018/1/12.
+ */
+public class SecureMethod {
+    public static final String KEY_ALGORTHM="RSA";
+    //public static final String SIGNATURE_ALGORITHM="MD5withRSA";
+    public static final String SIGNATURE_ALGORITHM="SHA256WithRSA";
+
+
+    public static final String PUBLIC_KEY = "RSAPublicKey";//公钥
+    public static final String PRIVATE_KEY = "RSAPrivateKey";//私钥
+    public static final String KEY_SHA="SHA";
+    public static final String KEY_MD5="MD5";
+
+
+    /**
+     * BASE64解密
+     * @param key
+     * @return
+     * @throws Exception
+     */
+    public static byte[] decryptBASE64(String key) throws Exception{
+        return (new BASE64Decoder()).decodeBuffer(key);
+    }
+
+    /**
+     * BASE64加密
+     * @param key
+     * @return
+     * @throws Exception
+     */
+    public static String encryptBASE64(byte[] key)throws Exception{
+        return (new BASE64Encoder()).encodeBuffer(key);
+    }
+
+    /**
+     * MD5加密
+     * @param data
+     * @return
+     * @throws Exception
+     */
+    public static byte[] encryptMD5(byte[] data)throws Exception{
+        MessageDigest md5 = MessageDigest.getInstance(KEY_MD5);
+        md5.update(data);
+        return md5.digest();
+    }
+    /**
+     * SHA加密
+     * @param data
+     * @return
+     * @throws Exception
+     */
+    public static byte[] encryptSHA(byte[] data)throws Exception{
+        MessageDigest sha = MessageDigest.getInstance(KEY_SHA);
+        sha.update(data);
+        return sha.digest();
+    }
+
+
+    /**
+     * 初始化密钥
+     * @return
+     * @throws Exception
+     */
+    public static Map<String,Object> initRSAKey()throws Exception{
+        KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance(KEY_ALGORTHM);
+        keyPairGenerator.initialize(512);
+        KeyPair keyPair = keyPairGenerator.generateKeyPair();
+
+        //公钥
+        RSAPublicKey publicKey = (RSAPublicKey) keyPair.getPublic();
+        //私钥
+        RSAPrivateKey privateKey =  (RSAPrivateKey) keyPair.getPrivate();
+
+        Map<String,Object> keyMap = new HashMap<String, Object>(2);
+        keyMap.put(PUBLIC_KEY, publicKey);
+        keyMap.put(PRIVATE_KEY, privateKey);
+
+        return keyMap;
+    }
+    /**
+     * 取得公钥，并转化为String类型
+     * @param keyMap
+     * @return
+     * @throws Exception
+     */
+    public static String getPublicKey(Map<String, Object> keyMap)throws Exception{
+        Key key = (Key) keyMap.get(PUBLIC_KEY);
+        return encryptBASE64(key.getEncoded());
+    }
+
+    /**
+     * 取得私钥，并转化为String类型
+     * @param keyMap
+     * @return
+     * @throws Exception
+     */
+    public static String getPrivateKey(Map<String, Object> keyMap) throws Exception{
+        Key key = (Key) keyMap.get(PRIVATE_KEY);
+        return encryptBASE64(key.getEncoded());
+    }
+
+    /**
+     * 用公钥加密
+     * @param data	加密数据
+     * @param key	密钥
+     * @return
+     * @throws Exception
+     */
+    public static byte[] encryptByPublicKey(byte[] data,String key)throws Exception{
+        //对公钥解密
+        byte[] keyBytes = decryptBASE64(key);
+        //取公钥
+        X509EncodedKeySpec x509EncodedKeySpec = new X509EncodedKeySpec(keyBytes);
+        KeyFactory keyFactory = KeyFactory.getInstance(KEY_ALGORTHM);
+        Key publicKey = keyFactory.generatePublic(x509EncodedKeySpec);
+
+        //对数据解密
+        Cipher cipher = Cipher.getInstance(keyFactory.getAlgorithm());
+        cipher.init(Cipher.ENCRYPT_MODE, publicKey);
+
+        return cipher.doFinal(data);
+    }
+
+    /**
+     * 用私钥解密 * @param data 	加密数据
+     * @param key	密钥
+     * @return
+     * @throws Exception
+     */
+    public static byte[] decryptByPrivateKey(byte[] data,String key)throws Exception{
+        //对私钥解密
+        byte[] keyBytes = decryptBASE64(key);
+
+        PKCS8EncodedKeySpec pkcs8EncodedKeySpec = new PKCS8EncodedKeySpec(keyBytes);
+        KeyFactory keyFactory = KeyFactory.getInstance(KEY_ALGORTHM);
+        Key privateKey = keyFactory.generatePrivate(pkcs8EncodedKeySpec);
+        //对数据解密
+        Cipher cipher = Cipher.getInstance(keyFactory.getAlgorithm());
+        cipher.init(Cipher.DECRYPT_MODE, privateKey);
+
+        return cipher.doFinal(data);
+    }
+
+    /**
+     * 用私钥加密
+     * @param data	加密数据
+     * @param key	密钥
+     * @return
+     * @throws Exception
+     */
+    public static byte[] encryptByPrivateKey(byte[] data,String key)throws Exception{
+        //解密密钥
+        byte[] keyBytes = decryptBASE64(key);
+        //取私钥
+        PKCS8EncodedKeySpec pkcs8EncodedKeySpec = new PKCS8EncodedKeySpec(keyBytes);
+        KeyFactory keyFactory = KeyFactory.getInstance(KEY_ALGORTHM);
+        Key privateKey = keyFactory.generatePrivate(pkcs8EncodedKeySpec);
+
+        //对数据加密
+        Cipher cipher = Cipher.getInstance(keyFactory.getAlgorithm());
+        cipher.init(Cipher.ENCRYPT_MODE, privateKey);
+
+        return cipher.doFinal(data);
+    }
+
+    /**
+     * 用公钥解密
+     * @param data	加密数据
+     * @param key	密钥
+     * @return
+     * @throws Exception
+     */
+    public static byte[] decryptByPublicKey(byte[] data,String key)throws Exception{
+        //对私钥解密
+        byte[] keyBytes = decryptBASE64(key);
+        X509EncodedKeySpec x509EncodedKeySpec = new X509EncodedKeySpec(keyBytes);
+        KeyFactory keyFactory = KeyFactory.getInstance(KEY_ALGORTHM);
+        Key publicKey = keyFactory.generatePublic(x509EncodedKeySpec);
+
+        //对数据解密
+        Cipher cipher = Cipher.getInstance(keyFactory.getAlgorithm());
+        cipher.init(Cipher.DECRYPT_MODE, publicKey);
+
+        return cipher.doFinal(data);
+    }
+
+
+    /**
+     *	用私钥对信息生成数字签名
+     * @param data	//加密数据
+     * @param privateKey	//私钥
+     * @return
+     * @throws Exception
+     */
+    public static String sign(byte[] data,String privateKey)throws Exception{
+        //解密私钥
+        byte[] keyBytes = decryptBASE64(privateKey);
+        //构造PKCS8EncodedKeySpec对象
+        PKCS8EncodedKeySpec pkcs8EncodedKeySpec = new PKCS8EncodedKeySpec(keyBytes);
+        //指定加密算法
+        KeyFactory keyFactory = KeyFactory.getInstance(KEY_ALGORTHM);
+        //取私钥匙对象
+        PrivateKey privateKey2 = keyFactory.generatePrivate(pkcs8EncodedKeySpec);
+        //用私钥对信息生成数字签名
+        Signature signature = Signature.getInstance(SIGNATURE_ALGORITHM);
+        signature.initSign(privateKey2);
+        signature.update(data);
+
+        return encryptBASE64(signature.sign());
+    }
+
+    /**
+     * 校验数字签名
+     * @param data	加密数据
+     * @param publicKey	公钥
+     * @param sign	数字签名
+     * @return
+     * @throws Exception
+     */
+    public static boolean verify(byte[] data,String publicKey,String sign)throws Exception{
+        //解密公钥
+        byte[] keyBytes = decryptBASE64(publicKey);
+        //构造X509EncodedKeySpec对象
+        X509EncodedKeySpec x509EncodedKeySpec = new X509EncodedKeySpec(keyBytes);
+        //指定加密算法
+        KeyFactory keyFactory = KeyFactory.getInstance(KEY_ALGORTHM);
+        //取公钥匙对象
+        PublicKey publicKey2 = keyFactory.generatePublic(x509EncodedKeySpec);
+
+        Signature signature = Signature.getInstance(SIGNATURE_ALGORITHM);
+        signature.initVerify(publicKey2);
+        signature.update(data);
+        //验证签名是否正常
+        return signature.verify(decryptBASE64(sign));
+
+    }
+
+
+
+    public static String generateAccessKey(){
+        String accessKey = CommonMethod.generateGuid();
+        accessKey = accessKey.substring(0,16);
+        StringBuilder stringBuilder = new StringBuilder();
+        Random random = new Random();
+        for(int i = 0; i < accessKey.length(); i++){
+            char c = accessKey.charAt(i);
+            if(CommonMethod.isNumberChar(c)) {
+                stringBuilder.append(c);
+                continue;
+            }
+            if(random.nextBoolean()){
+                stringBuilder.append(String.valueOf(c).toUpperCase());
+            }else{
+                stringBuilder.append(String.valueOf(c).toLowerCase());
+            }
+        }
+        return stringBuilder.toString();
+    }
+    public static String generateAccessSecret(){
+        String accessSecret = CommonMethod.generateGuid();
+        StringBuilder stringBuilder = new StringBuilder();
+        Random random = new Random();
+        for(int i = 0; i < accessSecret.length(); i++){
+            char c = accessSecret.charAt(i);
+            if(CommonMethod.isNumberChar(c)) {
+                stringBuilder.append(c);
+                continue;
+            }
+            if(random.nextBoolean()){
+                stringBuilder.append(String.valueOf(c).toUpperCase());
+            }else{
+                stringBuilder.append(String.valueOf(c).toLowerCase());
+            }
+        }
+        return stringBuilder.toString();
+    }
+}
